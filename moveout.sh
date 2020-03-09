fad2358957b6689c6812bb965420a47292072a3d
David Reyes <dreyes15@miners.utep.edu>
2020-03-09 21:17:09 +0000

    Adding scripts and new files for dotfiles

---
+++ moveout.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Staring moving dot-files from remote to your local..."
+
+echo "Moving .vimrc..."
+cp ./.vimrc ~/.
+echo "Done"
+
+echo "Moving vim folder .vim"
+cp -r ./.vim ~/.
+echo "Done"
+
+echo "Moving .ctags..."
+cp ./.ctags ~/.
+echo "Done"
+
+echo "Moving .gitconfig"
+cp ./.gitconfig ~/.
+echo "Done"
+
+echo "Moving .gitignore_global"
+cp ./.gitignore_global ~/.
+echo "Done"
+
+echo "Moving .zshrc..."
+cp ./.zshrc ~/.
+echo "Done"
+
+echo "Moving .tmux.conf..."
+cp ./.tmux.conf ~/.
+echo "Done"
+
+echo "Moving .tmux_theme.."
+cp ./.tmux_theme ~/.
+echo "Done"
+
+echo "Moving .dircolors..."
+cp ./.dircolors ./
+echo 'Done'
